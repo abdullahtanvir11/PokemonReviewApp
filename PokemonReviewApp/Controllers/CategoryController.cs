2234d75a81312c8f587634c538cf0be9c53268da
Abdullah Tanvir <abdullahtanvir872@gmail.com>
2024-01-12 17:44:52 +0500

    Get Set Part 2

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            
+        }
+    }
+}
